16f1a40c47e89e50ad0f7358f9baaf2b7cfe2b74
Marie Kauth <marie.kauth@gmail.com>
2022-03-22 21:39:22 -0400

    Nicole sent me

--- calculate_coins.js
+++ calculate_coins.js
@@ -1,3 +1,29 @@
 var $ = function (id) {
     return document.getElementById(id);
 }
+
+const calculate_click = function() {
+    var cents = $("cents").value;
+    var quarters = 0.25;
+    var nickels = 0.05;
+    var pennies = 0.01;
+    var dimes = 0.10;
+
+    pennies = cents % 1;
+    cents =  parseFloat("1");
+    $("pennies").value = pennies;
+
+    nickels = cents % 5;
+    cents = parseFloat 5);
+    $("nickels").value = nickels;
+
+    dimes = cents % 10;
+    cents = parseFloat("10");
+    $("dimes").value = dimes;
+
+    quarters = cents % 25;
+    cents = parseFloat("25")
+    $("quarters").value = quarters
+}
+
+$("calculate").addEventListener("click", calculate_click);
