992441b8ea658b5979f58e3de5a747fefce3565e
Nicole Thomas <Nicole09_2003@yahoo.com>
2022-03-23 22:31:26 -0400

    implemented DRY principle

--- calculate_coins.js
+++ calculate_coins.js
@@ -12,6 +12,11 @@ var calculate_click = function()
     coins.set("nickels", 5);
     coins.set("pennies", 1);
 
+    coins.forEach(function(value, key){
+        $(key).value = Math.floor(cents / value);
+        cents = cents % value;
+    });
+/*
     var quarters = Math.floor(cents / coins.get("quarters"));
     cents = cents % coins.get("quarters");
     $("quarters").value = quarters;
@@ -27,6 +32,7 @@ var calculate_click = function()
     var pennies = Math.floor(cents / coins.get("pennies"));
     cents = cents % coins.get("pennies");
     $("pennies").value = pennies;
+    */
 }
 
 $("calculate").addEventListener("click", calculate_click);
